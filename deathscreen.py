c845ca6ae534416c4acf578d0e490aa76318ec2e
Dani <132365319+DaniLP87@users.noreply.github.com>
2023-10-29 11:04:47 +0100

    Create deathscreen.py

---
+++ deathscreen.py
@@ -0,0 +1,24 @@
+import os
+os.system("clear")
+
+RED = "\033[31m"
+RESET = "\033[38;2;255;255;255m"
+# deathscreen
+print(RED+"                ")
+print(RED+"             _  ")
+print(RED+"           .' ) ")
+print(RED+" ,.--.    / .'  ")
+print(RED+"//    \  / /    ")
+print(RED+"\\    / / /     ")
+print(RED+" `'--' . '      ")
+print(RED+" ,.--. | |      ")
+print(RED+"//    \' '      ")
+print(RED+"\\    / \ \     ")
+print(RED+" `'--'   \ \    ")
+print(RED+"          \ '.  ")
+print(RED+"           '._) ")
+print(RED+"")
+print("An fatal error occured while running the OS")
+print("Press Enter to Restart The Computer")
+input()
+os.system("python3 main.py")
